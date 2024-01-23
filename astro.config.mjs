88e6c2d9d95a0930f54001ce4e691668c04aefec
Juanma Garrido <juanmagp26@gmail.com>
2024-01-24 00:44:34 +0100

    insalart netifly

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
@@ -14,10 +16,5 @@ export default defineConfig({
     /* ... */
   },
   // Añade esta sección
-  adapter: {
-    name: "@astrojs/adapter-netlify", // o el adaptador que estés utilizando
-    options: {
-      /* ... */
-    },
-  },
+  adapter: netlify()
 });
